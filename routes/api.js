1e2f9bd1bcd68f0602b83b384afb61016a0fd26e
ManueleDentello <manuele.dentello@gmail.com>
2023-08-26 00:59:15 +0200

    - aggiunta gestione user_name in session per tutte le api verso mongodb - aggiunta in /favorites della chiamata alla funzione db per ricevere tutti i preferiti dell'utente prima del render - imbastito esempio di utilizzo delle credenziali twitch con session

--- routes/api.js
+++ routes/api.js
@@ -15,10 +15,14 @@ async function get_twitch_stuff() {
     * Retrieve twitch_client_id and twitch_access_token from utils/twitch.js
     */
 
+
+    // HO CREATO UNA VERSIONE CON LA SESSION, FORSE STA ROBA NON SERVE PIU'
     const risultato = await authentication.auth_to_twitch();
 
     twitch_client_id = risultato.client_id;
     twitch_access_token = risultato.access_token;
+
+
 };
 
 get_twitch_stuff();
@@ -27,10 +31,11 @@ get_twitch_stuff();
  *  ATTENTION: these are api endpoints, not web endpoints. They are called by handlebars views
  *  flow: web link -> web endpoint in app.js -> games_ajax or game_ajax view -> api endpoint (igdb or db) -> query igdb -> HTML render
  */
-router.get('/best', async function(req, res, next) {
-  let games = await igdb.getBest(twitch_client_id, twitch_access_token);
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(games));
+router.get('/best', async function (req, res, next) {
+    // in caso di utilizzo della nuova route per igdb, posso usare la session per recuperare i dati aziché passarmeli sempre come parametri
+    let games = await igdb.getBest(twitch_client_id/*req.session.twitch_client_id*/, twitch_access_token/*req.session.twitch_client_secret*/);
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(games));
 });
 
 router.get('/popular', async function(req, res, next) {
@@ -46,16 +51,12 @@ router.get('/hype', async function(req, res, next) {
 });
 
 router.get('/favorites', async function(req, res, next) {
-    /*
-    let user_name = lo prendiamo dal session
-    let favorites = db.getFavorites(user_name);
-    let favoriteString = favorites.join(,);
-    */
+    let user_name = req.session.user_name;
+    let favoritesIds = db.getFavorites(user_name);
+    let favoriteIdsString = favoritesIds.join(',');
+    console.log('favorite games ids: ' + favoriteIdsString);
 
-    
-
-    //console.log("favorite games ids: " + req.query.id);
-    let favorites = await igdb.getFavorites(req.query.id/*favoriteString*/, twitch_client_id, twitch_access_token);  // req.query.id contains all favorite games ids
+    let favorites = await igdb.getFavorites(favoriteIdsString, twitch_client_id, twitch_access_token);  // favoriteIdsString contains all favorite games ids
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(favorites));
 });
@@ -74,8 +75,7 @@ router.get('/game/:id', async function(req, res, next) {
 
 //  MongoDB endpoints
 router.get('db/deleteFavorite/:id', async function(req, res, next) {
-    // prendo lo user id dalla session
-    // user_name o user_id = session.qualcosa
+    let user_name = req.session.user_name;
     let game_id = req.params.id;
     var result = db.deleteFavorite(user_name, game_id);
     res.setHeader("Content-Type", "application/json");
@@ -83,8 +83,7 @@ router.get('db/deleteFavorite/:id', async function(req, res, next) {
 });
 
 router.get('db/saveFavorite/:id', async function(req, res, next) {
-    // prendo lo user id dalla session
-    // user_name o user_id = session.qualcosa
+    let user_name = req.session.user_name;
     let game_id = req.params.id;
     var result = db.saveFavorite(user_name, game_id);
     res.setHeader("Content-Type", "application/json");
@@ -92,8 +91,7 @@ router.get('db/saveFavorite/:id', async function(req, res, next) {
 });
 
 router.get('db/getFavorite/:id', async function(req, res, next) {
-    // prendo lo user id dalla session
-    // user_name o user_id = session.qualcosa
+    let user_name = req.session.user_name;
     let game_id = req.params.id;
     var result = db.getFavorite(user_name, game_id);
     res.setHeader("Content-Type", "application/json");
