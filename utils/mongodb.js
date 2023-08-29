7593d06e327b229e9faa916b333081a443f69b09
ManueleDentello <manuele.dentello@gmail.com>
2023-08-29 19:10:27 +0200

    aggiornati return delle funzioni, cambiata stringa db

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -2,27 +2,31 @@ const mongoose = require('mongoose');
 require('./DB/favorites'); // cos'è sta roba?
 const saltRounds = 10; //Recommended by npmjs.com
 
-config = process.env.MONGODB;
-
-mongoose.connect(config);
+mongoose.connect(process.env.DB_CONNECT_STRING);
 
 module.exports = {
 
    saveFavorite: async function(user_name, game_id) {
       // create document
       const favorite = new mongoose.model('favorites')({
-         user_name: user_name,
+         username: user_name,
          game_id: game_id
       });
       // save document to db
       try {
+         // check if game is already saved
+         let cegia = await this.getFavorite(user_name, game_id);
+         if (cegia) {
+            return {saved: true};
+         }
+
          let newFavorite = await favorite.save();
          console.log('Document saved:', newFavorite);
-         return true;
+         return { saved: true };
       }
       catch (error) {
          console.error('Error during save:', error);
-         return false;
+         return { saved: false };
       }
    },
 
@@ -31,13 +35,13 @@ module.exports = {
       const favorite = new mongoose.model('favorites');
       try {
          // find a record with the same parameters
-         const game = await favorite.findOne({ user_name: user_name, game_id: game_id });
+         const game = await favorite.findOne({ username: user_name, game_id: game_id });
          if (game) {
-            console.log('Document found in the favorite games collection:', game);
-            return true;
+            console.log('Document found:', game);
+            return { favorite: true };
          } else {
-            console.log('No document found in the favorite games collection.');
-            return false;
+            console.log(`No document found for username ${user_name} and game_id ${game_id}`);
+            return { favorite: false };
          }
       } catch (err) {
          console.error('Error during getFavorite', err);
@@ -48,13 +52,13 @@ module.exports = {
       // prepare document
       const favorite = new mongoose.model('favorites');
       try {
-         const result = await favorite.deleteOne({ user_name: user_name, game_id: game_id });
+         const result = await favorite.deleteOne({ username: user_name, game_id: game_id });
          if (result.deletedCount > 0) {
-           console.log('Document with user_name ${user_name} and game_id ${game_id} has been cancelled from the favorite games collection.');
-           return true;
+           console.log(`Document with user_name ${user_name} and game_id ${game_id} has been cancelled from the favorite games collection.`);
+           return { deleted: true };
          } else {
-           console.log('No document with user_name ${user_name} and game_id ${game_id} was found in the favorite games collection.');
-           return false;
+           console.log(`No document with user_name ${user_name} and game_id ${game_id} was found in the favorite games collection.`);
+           return { deleted: true };
          }
        } catch (err) {
          console.error('Error during deleteFavorite', err);
@@ -65,14 +69,14 @@ module.exports = {
       // prepare document
       const query = new mongoose.model('favorites');
       try {
-         const favorites = await query.find({ user_name: user_name });
+         const favorites = await query.find({ username: user_name });
          if (favorites.length > 0) {
            const result = favorites.map(game => game.game_id);
-           console.log('Favorite game found for user_name ${user_name}:', result);
+           console.log(`Favorite game found for user_name ${user_name}:`, result);
            return result;
          } else {
-           console.log('No favorite games found for the user_name ${user_name}.');
-           return 0;
+           console.log(`No favorite games found for the user_name ${user_name}.`);
+           return null;
          }
        } catch (err) {
          console.error('Error during getFavorites', err);
