0c2749b8da77dd90eb7d548993710d3184af68d1
ManueleDentello <manuele.dentello@gmail.com>
2023-08-29 19:13:46 +0200

    impostata la session sicura

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(session({
     token: '',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false },  // to be modified
+    cookie: { secure: true },  // to be modified
   }))
 
 /*
