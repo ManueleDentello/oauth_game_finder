c3d017d49c582a404d4c769fa95376cc7ae15f31
ManueleDentello <manuele.dentello@gmail.com>
2023-08-29 19:10:10 +0200

    rimosse proprietà inutili

--- utils/DB/favorites.js
+++ utils/DB/favorites.js
@@ -3,14 +3,12 @@ const mongoose = require('mongoose');
 const favoriteSchema = new mongoose.Schema({
     username:{
       type: String,
-      unique: true,
       required: true,
-      lowercase: true
-    }, // Unique string representing the user
+    },
     game_id:{
       type: Number,
       required: true
-    } // Hash password of the user;
+    }
 });
 
 mongoose.model('favorites', favoriteSchema);
