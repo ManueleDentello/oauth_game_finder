02d2c2c3b389ff3a4d7355229b38f0574e728f33
ManueleDentello <manuele.dentello@gmail.com>
2023-09-13 00:36:30 +0200

    - revisione codice - implementato meglio endpoint /logout

--- routes/auth.js
+++ routes/auth.js
@@ -5,10 +5,12 @@ const axios = require('axios');
 const https = require('https');
 const crypto = require('crypto');
 const logger = require('../utils/logger');
-const { access } = require('fs');
 
 const callbackUrl = process.env.OAUTH_CALLBACK_URL;
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; // fix for 'Access Token Error Client request error: self-signed certificate' in /callback route
+const oauthServer = process.env.OAUTH_HOST;
+const EXPIRATION_WINDOW_IN_SECONDS = 300;
+const REFRESH_TOKEN_GRANT_TYPE = 'refresh_token';
 
 const config = new AuthorizationCode({
   client: {
@@ -22,25 +24,19 @@ const config = new AuthorizationCode({
   },
 });
 
-
 /*
-* NOT PART OF OAUTH FLOW (JUST FOR DEMO)
 * Client registration: server reroutes the request to OAuth server in order to get the form
-* no need to do it every time the app starts
+* no need to do it every time the app starts (values remain the same)
 */
 router.get('/client/register', async function (req, res, next) {
-  // Dovrebbe essere tutto molto simile a /user/register, solo che il client_id e 
-  // client_secret che ritorna vanno salvati in una variable globale come nel .env
-  // importante ricordare che non e' necessario farlo ogni avvio dell'app
-
   try {
-    // Creazione di un'istanza Axios con l'agente HTTPS personalizzato
+    // Axios instance with special header that permits self signed certificates
     const agent = new https.Agent({
-      rejectUnauthorized: false, // Consente certificati autofirmati
+      rejectUnauthorized: false, 
     });
 
-    // Effettua la chiamata al server su localhost:443
-    const response = await axios.get('https://localhost:443/client/register', { httpsAgent: agent });
+    // call OAuthServer
+    const response = await axios.get(oauthServer + '/client/register', { httpsAgent: agent });
     res.send(response.data);
 
   } catch (error) {
@@ -51,20 +47,19 @@ router.get('/client/register', async function (req, res, next) {
 });
 
 /*
-* NOT PART OF OAUTH FLOW (JUST FOR DEMO)
 * Client registration: intercepts compiled form and send data to OAuth server
+* no need to do it every time the app starts (values remain the same)
 */
 router.post('/client/register', async function (req, res, next) {
   try {
-    // Creazione di un'istanza Axios con l'agente HTTPS personalizzato
+    user = req.session.userName;
+
     const agent = new https.Agent({
-      rejectUnauthorized: false, // Consente certificati autofirmati
+      rejectUnauthorized: false,
     });
 
-    // Effettua la chiamata al server su localhost:443
-    const response = await axios.post('https://localhost:443/client/register', req.body, { httpsAgent: agent });  // req.body contains the compiled form
-    console.log('Risposta dal server:', response.data); // just to show that oauth server sends client id and client secret
-    res.redirect('/');
+    const response = await axios.post(oauthServer + '/client/register', req.body, { httpsAgent: agent });  // req.body contains the compiled form
+    res.render('message', { title: 'Registrazione client OAuth', message: JSON.stringify(response.data), user: user });
 
   } catch (error) {
     console.error('Error on POST /client/register to OAuth server:', error);
@@ -83,7 +78,7 @@ router.get('/user/register', async function (req, res, next) {
     });
 
     // Effettua la chiamata al server su localhost:443
-    const response = await axios.get('https://localhost:443/user/register', { httpsAgent: agent });
+    const response = await axios.get(oauthServer + '/user/register', { httpsAgent: agent });
     res.send(response.data);
 
   } catch (error) {
@@ -106,13 +101,13 @@ router.post('/user/register', async function (req, res, next) {
     });
 
     // Effettua la chiamata al server su localhost:443
-    const response = await axios.post('https://localhost:443/user/register', req.body, { httpsAgent: agent });
+    const response = await axios.post(oauthServer + '/user/register', req.body, { httpsAgent: agent });
     
     logger.debug(`Risposta dal server oauth creazione utente: ` + response.data);
 
     logger.info('Utente registrato con successo');
 
-    res.render('message', { title: 'Risposta server oauth', message: response.data, redirect: '' });
+    res.render('message', { title: 'Risposta server oauth', message: response.data, user: req.session.userName });
 
   } catch (error) {
     logger.error('Errore durante la chiamata POST al server per la registrazione utente:', error);
@@ -123,7 +118,7 @@ router.post('/user/register', async function (req, res, next) {
 /*
 * request authorization code to oauth server
 */
-router.get('/user/authorize', async(req, res) => {
+router.get('/login', async(req, res) => {
   const authorizationUri = config.authorizeURL({
     redirect_uri: callbackUrl, 
     response_type: 'code',
@@ -138,7 +133,7 @@ router.get('/user/authorize', async(req, res) => {
 */
 router.get('/callback', async (req, res) => {
   const { code } = req.query; // create a variable that stores the value of "code" query parameter
-  console.log('Quello che c\'è in req: \n' + req.body);
+  //console.log('Quello che c\'è in req: \n' + req.body);
   const options = {
     code,
     redirect_uri: callbackUrl
@@ -146,22 +141,18 @@ router.get('/callback', async (req, res) => {
 
   try {
     const accessToken = await config.getToken(options);
+    logger.info('Contenuto del token: ' + JSON.stringify(accessToken));
 
-    accessToken.expired();
-
-    // Aggiungo token di sessione alla sessione corrente
-    //req.session.token = accessToken.token.access_token;
-
-    // Salvo tutto quello che ritorna il server oauth nella sessione corrente
+    // Save the entire token in the session. Attention: the data is the accessToken.token object
     req.session.oauth_token = accessToken.token;
 
-    logger.info("Ottenuto access token: " + accessToken.token.access_token);
+    logger.info('Token: ' + req.session.oauth_token.access_token);
 
     // Mando utente verso redirect per prendere lo username
     res.redirect('/username');
 
   } catch (error) {
-    console.error('Access Token Error', error.message);
+    logger.error('Error during the retrieval of the token: ', error.output);
     res.status(500).json('Authentication failed');
   }
 });
@@ -173,12 +164,14 @@ router.get('/username', async (req, res) => {
       rejectUnauthorized: false, // Consente certificati autofirmati
     });
 
+    logger.info('access_token ', req.session.oauth_token.access_token);
+
     const headers = {
       'Authorization': 'Bearer ' + req.session.oauth_token.access_token
     };
 
     // Effettua la chiamata al server su localhost:443
-    const response = await axios.get('https://localhost:443/oauth/username', { headers: headers }, { httpsAgent: agent });
+    const response = await axios.get(oauthServer + '/oauth/username', { headers: headers }, { httpsAgent: agent });
 
     // Salvo lo username nella sessione corrente
     req.session.userName = response.data;
@@ -187,7 +180,7 @@ router.get('/username', async (req, res) => {
     // Mando l'utente nella home - fine autenticazione
     res.redirect('/');
   } catch (error) {
-    logger.error('Errore durante la chiamata POST al server per l\'accesso alla risorsa protetta (username):', error);
+    logger.error('Errore durante la chiamata GET al server per l\'accesso alla risorsa protetta (username):', error);
     res.status(500).send('Errore durante la chiamata al server oauth2');
   }
 });
@@ -196,46 +189,53 @@ router.get('/username', async (req, res) => {
 * request a new token to oauth server when the old one expires
 */
 router.get('/refresh-token', async (req, res) => {
-  const accessToken = await config.createToken(req.session.oauth_token);
-  const EXPIRATION_WINDOW_IN_SECONDS = 3599;
+  let accessToken = await config.createToken(req.session.oauth_token);
     
-  if(accessToken.expired(EXPIRATION_WINDOW_IN_SECONDS)) {
+  //if(accessToken.expired(EXPIRATION_WINDOW_IN_SECONDS)) {
     try {
       const refreshParams = {
         refresh_token: req.session.oauth_token.refresh_token,
-        grant_type: 'refresh_token',
+        grant_type: REFRESH_TOKEN_GRANT_TYPE,
         redirect_uri: callbackUrl
       };
 
       const result = await accessToken.refresh(refreshParams);
       
-      // Salvo tutto quello che ritorna il server oauth nella sessione corrente
+      // Save internally all info coming from the OAuth server
       req.session.oauth_token = result.token;
 
-      logger.info('Token refreshato');
+      logger.info('Token refreshed');
 
-      res.send('Access token refreshed');
+      //res.send('Access token refreshed');
+      next();
     } catch (error) {
-      console.error('Error refreshing access token:', error.message);
+      console.error('Error refreshing access token:', error.output);
       res.status(500).send('Error refreshing access token');
     }
-  }
-});
-router.get('/login', async function (req, res, next) {
-  res.redirect('/user/authorize'); 
+  //}
 });
 
 router.get('/logout', async function (req, res, next) {
-  if(!req.session.token){
-    res.render('message', { title: 'Logout', message: 'Devi essere loggato per effettuare il logout', redirect: '/login' });
+  let userName = req.session.userName;
+
+  if(!req.session.oauth_token){
+    res.render('message', { title: 'Logout', message: 'Devi essere loggato per effettuare il logout', user: userName });
   } else {
-    req.session.userName = '';
-    //req.session.oauth_token;
-    res.render('message', { title: 'Logout', message: 'Logout effettuato correttamente', redirect: '' });
+    // delete all data in session and set a blank username
+    userName = '';
+    req.session.destroy();
+
+    // Revokes both tokens, refresh token is only revoked if the access_token is properly revoked
+    let accessToken = await config.createToken(req.session.oauth_token);
+    try {
+      await accessToken.revokeAll();
+      logger.info('Access token and refresh token deleted');
+    } catch (error) {
+      logger.error('Error revoking token: ', error.message);
   }
 
-  // revocare token, auth code e quant'altro?
-  // chiudere connessione al database
+    res.render('message', { title: 'Logout', message: 'Logout effettuato correttamente', user: userName });
+  }
 });
 
 router.get('/userinfo', (req, res) => {
