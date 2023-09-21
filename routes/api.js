0c8a58053c85b619b19692565f3c493b6a67701f
ManueleDentello <manuele.dentello@gmail.com>
2023-09-21 23:36:13 +0200

    code cleanup + logging

--- routes/api.js
+++ routes/api.js
@@ -5,8 +5,8 @@ const logger = require('../utils/logger');
 const authentication = require('../utils/twitch.js');
 const router = express.Router();
 
-var twitchClientID;
-var twitchAccessToken;
+let twitchClientID;
+let twitchAccessToken;
 
 /* 
  * Retrieve twitch_client_id and twitch_access_token from utils/twitch.js
@@ -27,22 +27,23 @@ getTwitchCredentials();
  */
 router.get('/best', async function (req, res, next) {
     let games = await igdb.getBest(twitchClientID, twitchAccessToken);
+    logger.debug('JSON response from IGDB for /best: ' + games);
 
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(games));
 });
 
 router.get('/popular', async function(req, res, next) {
-
     let games = await igdb.getPopular(twitchClientID, twitchAccessToken);
+    logger.debug('JSON response from IGDB for /popular: ' + games);
 
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(games));
 });
 
 router.get('/hype', async function(req, res, next) {
-
     let games = await igdb.getHype(twitchClientID, twitchAccessToken);
+    logger.debug('JSON response from IGDB for /hype: ' + games);
 
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(games));
@@ -56,20 +57,21 @@ router.get('/favorites', async function(req, res, next) {
     if (favoritesIDsArray != null) {
         let favoriteIDsString = favoritesIDsArray.join(',');
         favoritesJSON = await igdb.getFavorites(favoriteIDsString, twitchClientID, twitchAccessToken);  // favoriteIdsString contains all favorite games ids
+        
     }
     else
         favoritesJSON = '';
 
-    logger.debug('Favorites: ', favoritesJSON);
-
+    logger.debug('JSON response from IGDB for /favorites: ' + favoritesJSON);
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(favoritesJSON));
 });
 
 router.get('/search', async function(req, res, next) {
-    const keywordSearched = req.query.txtRicerca;
+    const keyword = req.query.txtRicerca;
 
-    let output = await igdb.getSearched(keywordSearched, twitchClientID, twitchAccessToken);
+    let output = await igdb.getSearched(keyword, twitchClientID, twitchAccessToken);
+    logger.debug('JSON response from IGDB for /search: ' + output);
 
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(output));
@@ -79,6 +81,7 @@ router.get('/game/:id', async function(req, res, next) {
     const gameID = req.params.id;
 
     let game = await igdb.getGame(gameID, twitchClientID, twitchAccessToken);
+    logger.debug('JSON response from IGDB for /game: ' + game);
 
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(game));
@@ -92,6 +95,7 @@ router.get('/deleteFavorite/:id', async function(req, res, next) {
     const gameID = req.params.id;
 
     let result = await db.deleteFavorite(userName, gameID);
+    logger.debug('Response from MongoDB for /deleteFavorite: ' + result);
 
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(result));
@@ -102,6 +106,7 @@ router.get('/saveFavorite/:id', async function(req, res, next) {
     const gameID = req.params.id;
 
     let result = await db.saveFavorite(userName, gameID);
+    logger.debug('Response from MongoDB for /saveFavorite: ' + result);
 
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(result));
@@ -112,6 +117,7 @@ router.get('/getFavorite/:id', async function(req, res, next) {
     const gameID = req.params.id;
 
     let result = await db.getFavorite(userName, gameID);
+    logger.debug('Response from MongoDB for /getFavorite: ' + result);
 
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(result));
